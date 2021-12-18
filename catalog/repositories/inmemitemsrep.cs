92920ddc9d3a0d9c0252719429bb00ffd38b5452
KR45 <36817376+KR45@users.noreply.github.com>
2021-12-18 18:01:53 +0530

    Add files via upload

---
+++ catalog/repositories/inmemitemsrep.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using catalog.entities;
+
+namespace catalog.repositories
+{
+    public class inmemitemsrep
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item {Id= Guid.NewGuid(), Name="Potion", Price= 10, CreatedDate=DateTimeOffset.UtcNow},
+            new Item {Id= Guid.NewGuid(), Name="Arm", Price= 20, CreatedDate=DateTimeOffset.UtcNow},
+            new Item {Id= Guid.NewGuid(), Name="Duck", Price= 30, CreatedDate=DateTimeOffset.UtcNow},
+            new Item {Id= Guid.NewGuid(), Name="Iron", Price= 40, CreatedDate=DateTimeOffset.UtcNow}
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return  items.Where(item => item.Id == id).SingleOrDefault();
+        }
+
+    }
+}
