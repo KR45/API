ad06f1028526e98a541842fadf7fa22e7c3421a3
KR45 <36817376+KR45@users.noreply.github.com>
2021-12-21 21:32:24 +0530

    Update Startup.cs

--- catalog/Startup.cs
+++ catalog/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using catalog.repositories;
 
 namespace catalog
 {
@@ -26,6 +27,7 @@ namespace catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<itemrepos, inmemitemsrep>(); // adding service for retriving the object 
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
